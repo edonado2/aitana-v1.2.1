07a4251d6a21942b21875533950e11350c534f37
Ernesto <83133501+edonado2@users.noreply.github.com>
2023-03-05 21:31:06 -0400

    delete files

--- auth/Home.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
-
-export default class HomeScreen extends Component {
-  render() {
-    return <Text>Hello From HomeScreen</Text>;
-  }
-}
